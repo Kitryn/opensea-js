e42d9dcb79d2f1bbc4229e60d90be5e419b35d9a
Kitryn <snowfennard@gmail.com>
2021-08-13 16:33:11 +0800

    cleaned ./globals.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,17 +2,17 @@
 // Should be top-level
 
 // Taken from wyvern and 0x
-declare module "web3_beta";
-declare module "web3-provider-engine";
-declare module "web3-provider-engine/subproviders/rpc";
+// declare module "web3_beta";
+// declare module "web3-provider-engine";
+// declare module "web3-provider-engine/subproviders/rpc";
 
-/* tslint:enable */
-declare module "*.json" {
-  const json: any;
-  /* tslint:disable */
-  export default json;
-  /* tslint:enable */
-}
+// /* tslint:enable */
+// declare module "*.json" {
+//   const json: any;
+//   /* tslint:disable */
+//   export default json;
+//   /* tslint:enable */
+// }
 
 declare module "ethereumjs-abi" {
   const soliditySHA3: (argTypes: string[], args: any[]) => Buffer;
